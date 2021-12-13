d53a5c5624741bb24bdfc122f1684cf42cb0f7a8
JohanAndres19 <johanandresa@hotmail.com>
2021-12-13 10:21:06 -0500

    listo

--- static/js/generar_filtros.js
+++ static/js/generar_filtros.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
         }
         if(contador>=1){
             $('#pruebab').show(100);
-            console.log(contador)
+            console.log(contador,lista)
         }
         
     });
